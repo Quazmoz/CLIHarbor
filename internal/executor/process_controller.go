29d1db5b5db295f997dfb1666c46f7a8678a4fdf
Quinn <32716387+Quazmoz@users.noreply.github.com>
2026-09-18 15:42:02 +0100

    Delegate executor lifecycle ownership to shared controller

--- internal/executor/process_controller.go
+++ internal/executor/process_controller.go
@@ -1,6 +1,10 @@
 package executor
 
-import "os/exec"
+import (
+	"os/exec"
+
+	"github.com/Quazmoz/CLIHarbor/internal/processcontrol"
+)
 
 type processController interface {
 	configure(*exec.Cmd) error
@@ -10,3 +14,31 @@ type processController interface {
 }
 
 type processControllerFactory func() (processController, error)
+
+type sharedProcessController struct {
+	inner processcontrol.Controller
+}
+
+func newProcessController() (processController, error) {
+	controller, err := processcontrol.New()
+	if err != nil {
+		return nil, err
+	}
+	return sharedProcessController{inner: controller}, nil
+}
+
+func (c sharedProcessController) configure(cmd *exec.Cmd) error {
+	return c.inner.Configure(cmd)
+}
+
+func (c sharedProcessController) afterStart(cmd *exec.Cmd) error {
+	return c.inner.AfterStart(cmd)
+}
+
+func (c sharedProcessController) cancel(cmd *exec.Cmd) error {
+	return c.inner.Cancel(cmd)
+}
+
+func (c sharedProcessController) close() error {
+	return c.inner.Close()
+}
