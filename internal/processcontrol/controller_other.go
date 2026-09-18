9435c6ef2ffc254b42ec6190a7abc93650bee259
Quinn <32716387+Quazmoz@users.noreply.github.com>
2026-09-18 15:45:18 +0100

    Apply final Go formatting to process controller

--- internal/processcontrol/controller_other.go
+++ internal/processcontrol/controller_other.go
@@ -13,7 +13,7 @@ func newController() (Controller, error) {
 	return directController{}, nil
 }
 
-func (directController) Configure(*exec.Cmd) error { return nil }
+func (directController) Configure(*exec.Cmd) error  { return nil }
 func (directController) AfterStart(*exec.Cmd) error { return nil }
 
 func (directController) Cancel(cmd *exec.Cmd) error {
